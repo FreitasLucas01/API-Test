b712e99c5dffa0138e0eb63980269029c63655ab
Lucas Freitas <lucasfreitasdesa02@gmail.com>
2025-06-16 13:16:03 -0300

    refactor: Melhora a qualidade do código

--- 50 - Finalização API Operações básicas/func.js
+++ 50 - Finalização API Operações básicas/func.js
@@ -1,17 +1,29 @@
 function sum (a, b){
+    if(isNaN(a) || isNaN(b)) {
+        throw new Error("Please include numbers only")
+    }
     return a + b;
 }
 
 function sub(a, b){
+    if(isNaN(a) || isNaN(b)) {
+        throw new Error("Please include numbers only")
+    }
     return a - b;
 }
 
 function mult(a, b){
+    if(isNaN(a) || isNaN(b)) {
+        throw new Error("Please include numbers only")
+    }
     return a * b;
 }
 
 function div(a, b){
-    if (b === 0) {
+    if(isNaN(a) || isNaN(b)) {
+        throw new Error("Please include numbers only")
+    }
+    else if (b === 0) {
         throw new Error("Division by zero is not allowed");
     }
     return a / b;
